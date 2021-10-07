962d7987bab2c27712516eb8815c6b5689d4968a
Vico <vinicomelo@gmail.com>
2021-10-07 18:29:25 -0400

    test

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,23 @@
-import * as functions from "firebase-functions";
+import * as functions from 'firebase-functions';
+import * as express from 'express';
+import * as admin from 'firebase-admin';
+
+
+const app = express();
+admin.initializeApp();
+
+const db = admin.firestore();
+
+app.get('/', async (req, res) => {
+  res.send(JSON.stringify('testing'));
+});
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
-export const helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  response.send(JSON.stringify("Hello from Firebase!"));
+// eslint-disable-next-line max-len
+export const helloWorld = functions.https.onRequest(async (request, response) => {
+  await db.collection('entries').doc().create({name: 'vico'});
+  functions.logger.info('Hello logs!', {structuredData: true});
+  response.send(JSON.stringify('Hello from Firebase!'));
 });
