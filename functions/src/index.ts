db40f4af6b1635491e156ce95eee469e77f10cbf
Vico <vinicomelo@gmail.com>
2021-10-07 19:54:32 -0400

    clean up

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -10,15 +10,4 @@ const app = express();
 app.post('/entries', addMemory);
 app.get('/entries', getAllMemories);
 
-
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// eslint-disable-next-line max-len
-// export const helloWorld = functions.https.onRequest(async (request, response) => {
-//   await db.collection('entries').doc().create({name: 'vico'});
-//   functions.logger.info('Hello logs!', {structuredData: true});
-//   response.send(JSON.stringify('Hello from Firebase!'));
-// });
-
 exports.app = functions.https.onRequest(app);
